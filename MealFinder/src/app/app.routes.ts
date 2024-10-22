50db2a69760ca60ad2a006bedb5ed2381af491ce
Kian <S00237055@atu.ie>
2024-10-22 10:32:12 +0100

    save

--- MealFinder/src/app/app.routes.ts
+++ MealFinder/src/app/app.routes.ts
@@ -5,10 +5,12 @@ import { AboutComponent } from './about/about.component';
 import { LocationComponent } from './location/location.component';
 import { AllergiesComponent } from './allergies/allergies.component';
 
+
 export const routes: Routes = [
     {path:'Home',component:AppComponent},
     {path:'sign-in',component:SignInComponent},
     {path:'about',component:AboutComponent},
     {path:'location',component:LocationComponent},
+    {path: '', redirectTo: '/location', pathMatch: 'full'},
     {path:'allergies',component:AllergiesComponent}
 ];
--- MealFinder/src/app/location/location.component.ts
+++ MealFinder/src/app/location/location.component.ts
@@ -1,14 +1,22 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 import { CommonModule } from '@angular/common';
+import { GoogleMapsModule } from '@angular/google-maps';
 
 @Component({
   selector: 'app-location',
   standalone: true,
-  imports: [RouterOutlet, CommonModule, RouterLink, RouterLinkActive],
+  imports: [RouterOutlet, CommonModule, RouterLink, RouterLinkActive, GoogleMapsModule],
   templateUrl: './location.component.html',
   styleUrl: './location.component.css'
 })
 export class LocationComponent {
 
+  center: google.maps.LatLngLiteral = { lat: 37.7749, lng: -122.4194};
+  zoom = 12;
+
+  // options: google.maps.MapOptions = {
+  //   maxZoom: 18,
+  //   minZoom: 8,
+  // };
 }
