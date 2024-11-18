bf593b429001aef338fab81aa3b6f838613a6765
Kian <S00237055@atu.ie>
2024-11-18 23:06:26 +0000

    x

--- MealFinder/src/app/allergies/allergies.component.ts
+++ MealFinder/src/app/allergies/allergies.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { FormBuilder, FormGroup, FormArray, Validators, ReactiveFormsModule, FormsModule} from '@angular/forms'
+import { ApiService } from '../api.service';
+import { OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-allergies',
@@ -10,36 +12,67 @@ import { FormBuilder, FormGroup, FormArray, Validators, ReactiveFormsModule, For
   templateUrl: './allergies.component.html',
   styleUrl: './allergies.component.css'
 })
-export class AllergiesComponent {
-  allergyForm: FormGroup;
+export class AllergiesComponent implements OnInit {
+  //allergyForm: FormGroup;
+  searchQuery: string = "";
+  recipes: any[] = [];
 
-  constructor(private fb: FormBuilder) {
-    this.allergyForm = this.fb.group({
-      username: ['', Validators.required],
-      email: ['', Validators.required, Validators.email],
-      password: ['', Validators.required, Validators.minLength(6)], 
-      allergies: this.fb.array([])
+  constructor(
+    //private fb: FormBuilder,
+    private apiService: ApiService
+  ) {}
+
+  ngOnInit(): void {}
+
+  searchRecipes(): void {
+    this.apiService.searchRecipes(this.searchQuery).subscribe({
+      next: (data) => {
+        this.recipes = data.results;
+        console.log("Recipes:", this.recipes);
+      },
+      error: (err) => {
+        console.error("Error fetching recipes:",err);
+      },
     });
   }
 
-  get allergies(): FormArray {
-    return this.allergyForm.get('allergies') as FormArray;
-  }
 
-  addAllergy(): void {
-    this.allergies.push(this.fb.control('', Validators.required));
 
-  }
 
-  removeAllergy(index: number): void {
-    this.allergies.removeAt(index);
-  }
 
-  onSubmit(): void {
-    if (this.allergyForm.valid) {
-      console.log('Form Submitted', this.allergyForm.value);
-    } else {
-      console.log('Form is not valid');
-    }
-  }
+
+
+
+
+
+
+
+    // this.allergyForm = this.fb.group({
+    //   username: ['', Validators.required],
+    //   email: ['', Validators.required, Validators.email],
+    //   password: ['', Validators.required, Validators.minLength(6)], 
+    //   allergies: this.fb.array([])
+    // });
+  
+
+  // get allergies(): FormArray {
+  //   return this.allergyForm.get('allergies') as FormArray;
+  // }
+
+  // addAllergy(): void {
+  //   this.allergies.push(this.fb.control('', Validators.required));
+
+  // }
+
+  // removeAllergy(index: number): void {
+  //   this.allergies.removeAt(index);
+  // }
+
+  // onSubmit(): void {
+  //   if (this.allergyForm.valid) {
+  //     console.log('Form Submitted', this.allergyForm.value);
+  //   } else {
+  //     console.log('Form is not valid');
+  //   }
+  // }
 }
--- MealFinder/src/app/api.service.ts
+++ MealFinder/src/app/api.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
+import { HttpErrorResponse } from '@angular/common/http';
+import { throwError } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -10,11 +12,15 @@ export class ApiService {
 
   private baseUrl = "https://api.edamam.com/search?q=REQUIRED&app_id=2bee36e1&app_key=66640312dfa9edb8093c37a1c19f766f";
 
-  constructor(private http: HttpClient) { }
 
-  getItems(): Observable<any> {
-    return this.http.get(`${this.baseUrl}`);
+  private handleError(err:HttpErrorResponse){
+    console.log('OmdbApiService:'+err.message);
+    return throwError(() => new Error("OmdbApiService:" + err.message))
   }
 
+  constructor(private http: HttpClient) { }
 
+  searchRecipes(query: string): Observable<any> {
+    return this.http.get(`${this.baseUrl}?q=${query}`);
+  } 
 }
