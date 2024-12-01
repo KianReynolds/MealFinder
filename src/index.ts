6b19809137c35c17f6ecc9d4864a0eb7cc6e7a61
Kian <S00237055@atu.ie>
2024-12-01 09:13:31 +0000

    2

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,11 @@
 import express, {Application, Request, Response} from "express" ;
 import morgan from "morgan";
 import userRoutes from '../routes/users';
+import dotenv from "dotenv";
+
+
+dotenv.config();
+
 const PORT = process.env.PORT || 3001;
 
 const app: Application = express();
@@ -9,17 +14,16 @@ app.use(morgan("tiny"));
 
 app.use(express.json());
 
-app.use('api/v1/users', userRoutes)
+app.use('/api/v1/users', userRoutes)
 
 app.get("/ping", async (_req : Request, res: Response) => {
     res.send({
     message: "hello from Kian",
     });
 });
-
-
-// app.get("/bananas", async (_req : Request, res: Response) =>
-//     res.send("hello world, this is bananas"));
+app.get('/bananas', async(_req : Request, res: Response) => {
+    res.send('hello world, this is bananas');
+});
 
  app.listen(PORT, () => {
     console.log("Server is running on port  --", PORT);
