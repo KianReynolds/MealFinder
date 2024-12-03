9c5ba09dfc9c6bbe66281f6eb78bd42911f434d1
Kian <S00237055@atu.ie>
2024-12-03 08:23:35 +0000

    changed the model to fit the frontend

--- models/meal.ts
+++ models/meal.ts
@@ -1,10 +1,33 @@
 import { ObjectId } from "mongodb";
 
-export interface Meal {
-    id?: ObjectId;
-    name: string;
-    category: string;
-    instructions: string;
-    ingredients: string[];
+// export interface Meal {
+//     id?: ObjectId;
+//     name: string;
+//     category: string;
+//     instructions: string;
+//     ingredients: string[];
+//   }
+  
+
+  export interface Meal {
+    idMeal: string;
+    strMeal: string;
+    strCategory: string;
+    strArea: string;
+    strInstructions: string;
+    strMealThumb: string;
+    strIngredient1: string;
+    strIngredient2: string;
+    strIngredient3: string;
+    strIngredient4: string;
+    strIngredient5: string;
+    strIngredient6: string;
+    strIngredient7: string;
+    strIngredient8: string;
+    strIngredient9: string;
   }
   
+  export interface themealdbResponse {
+    meals: Meal[];
+    Error:string;
+  }
