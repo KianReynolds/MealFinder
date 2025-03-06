038f9ad3a3cd96498fd2ee8b2dd3e4e8b694fb61
Ryan <s00237889@atu.ie>
2025-03-06 12:31:09 +0000

    added some forms and sections in the account managment page for favourite recipes,user details and food preferences, still have to populate this page with info from the DB

--- MealFinder/src/app/app.routes.ts
+++ MealFinder/src/app/app.routes.ts
@@ -4,10 +4,14 @@ import { AboutComponent } from './about/about.component';
 import { LocationComponent } from './location/location.component';
 import { HomeComponent } from './home/home.component';
 import { AuthComponent } from './auth/auth.component';
+import { AccountDetailsComponent } from './account-details/account-details.component';
+
+
 export const routes: Routes = [
     {path:'', component:HomeComponent},
     {path: 'home', redirectTo: '/'},
     {path:'about',component:AboutComponent},
     {path:'location',component:LocationComponent},
     {path: 'auth', component:AuthComponent},
+    {path: 'accountdetails', component:AccountDetailsComponent}
 ];
